60d58fdd2917b228d3909912ebb64e76303324ca
phvdat <dat.pham@lfglobaltech.com>
2024-05-03 14:59:16 +0700

    feat: add button go to home page

--- src/app/management-media/ManagementMedia.tsx
+++ src/app/management-media/ManagementMedia.tsx
@@ -1,16 +1,22 @@
 'use client';
-import UploadAudio from "@/components/management-media/UploadAudio";
-import UploadImage from "@/components/management-media/UploadImage";
-import { Typography } from "antd";
+import UploadAudio from '@/components/management-media/UploadAudio';
+import UploadImage from '@/components/management-media/UploadImage';
+import { HeartFilled } from '@ant-design/icons';
+import { Button, Typography } from 'antd';
+import { useRouter } from 'next/navigation';
 const { Title } = Typography;
 
 export const ManagementMedia = () => {
-    return (
-        <div>
-            <Title level={2}>Management Audio</Title>
-            <UploadAudio />
-            <Title level={2}>Management Images</Title>
-            <UploadImage />
-        </div>
-    );
-}
+  const router = useRouter();
+  return (
+    <div style={{ width: '100%', textAlign: 'center', padding: '20px 20px' }}>
+      <Button onClick={() => router.push('/')}>
+        <HeartFilled />
+      </Button>
+      <Title level={2}>Management Audio</Title>
+      <UploadAudio />
+      <Title level={2}>Management Images</Title>
+      <UploadImage />
+    </div>
+  );
+};
